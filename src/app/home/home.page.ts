098e1005e19ce33e08a500ebb57045b6fbbb93b8
Emeka Chukwu <emekac@nameksolutions.com>
2019-07-20 00:51:19 +0200

    update with pipes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,19 +13,22 @@ notes = [
     id: '1',
     date: '2018-03-09', //yyyy-mm-dd
     title: 'Ionic 5',
-    content: 'Seting up Ionic notes project.'
+    content: 'Seting up Ionic notes project.',
+    price: '23'
   },
   {
     id: '2',
     date: '2017-01-02',
     title: 'Angular',
-    content: 'Learning Routing in Angular'
+    content: 'Learning Routing in Angular',
+    price: '50'
   },
   {
     id: '3',
     date: '2019-11-07',
     title: 'Firebase',
-    content: 'This will be used to build the backend and storage.'
+    content: 'This will be used to build the backend and storage.',
+    price: '10'
   }
 ]
 
