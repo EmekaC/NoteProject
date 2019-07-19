4d5589d16fff0031d7b1fdeda676c73297f52734
Emeka Chukwu <emekac@nameksolutions.com>
2019-07-20 00:13:10 +0200

    Adding notes array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+notes = [
+  {
+    id: '1',
+    date: '2018-03-09', //yyyy-mm-dd
+    title: 'Ionic 5',
+    content: 'Seting up Ionic notes project.'
+  },
+  {
+    id: '2',
+    date: '2017-01-02',
+    title: 'Angular',
+    content: 'Learning Routing in Angular'
+  },
+  {
+    id: '3',
+    date: '2019-11-07',
+    title: 'Firebase',
+    content: 'This will be used to build the backend and storage.'
+  }
+]
 
 }
+
+
